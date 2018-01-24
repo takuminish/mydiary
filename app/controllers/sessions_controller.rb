c8691b33e8dd8e341eef9541184bb3f978e95eaa
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-01-24 22:07:27 +0900

    サインイン時にメッセージ表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
+      flash[:success] =  "#{user.name}さんでサインインしました。"
       signin user
       redirect_to signin_path
       

b64a34598ae9f1fc9e897e3637512708211d2203
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-04-20 01:01:22 +0900

    diaryのshowページを作成

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -4,5 +4,8 @@ class DiariesController < ApplicationController
     user = User.find_by(id: session[:user_id])
     @diary = Diary.where(user_id: user.id)
   end
-    
+
+  def show
+    @diary = Diary.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users
 
   get    'diarylist' => 'diaries#index'
+  resources :diaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

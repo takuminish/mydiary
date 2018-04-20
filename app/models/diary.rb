3ca39fab13e9f59f3c26d13f1b41615c931440ec
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-04-21 00:48:48 +0900

    gem redcarpetを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -4,6 +4,6 @@ class Diary < ApplicationRecord
   belongs_to :user 
 
   validates :statement, presence: true
-  validates :display_date, presence: true , format: {with: /[0-9]{4}\.[0-1][0-9]\.[0-3][0-9]\([月火水木金土]\)/}
+  validates :display_date, presence: true, format: {with: /[0-9]{4}\.[0-1][0-9]\.[0-3][0-9]\([月火水木金土]\)/}
 
 end

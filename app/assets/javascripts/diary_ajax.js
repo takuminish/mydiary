fe5ed289e6d1d11be856b0bb7ee2fe0160550a1f
takuminish <s15t253@stu.kagawa-u.ac.jp>
2018-06-25 21:56:39 +0900

    エンターキーで検索機能を発火させる

--- app/assets/javascripts/diary_ajax.js
+++ app/assets/javascripts/diary_ajax.js
@@ -3,6 +3,12 @@ $(document).on('turbolinks:load', function() {
 	$('.preview-diary').html(marked($('#diary-statement').val()));  
     });
 
+    $('#diary_search_date').on('keydown', function(e) {
+        if (e.which == 13) {
+	    $('#diary_search_date_button').trigger('click');
+	}
+    });
+    
     $('#diary_search_date_button').on("click", function() {
 	var search_text = $.trim($('#diary_search_date').val());
 	$.ajax({ 
@@ -14,10 +20,10 @@ $(document).on('turbolinks:load', function() {
 	    dataType: 'json' 
 	}).done(function(data){
 	    $('.diary-list').find('.row').fadeOut().promise().done(function() {
-//		$('.diary-list').append('<div class="row">').hide().fadeIn();
-		$('<div class="row">').appendTo('.diary-list').hide().addClass('is-animated').fadeIn(1000);
-	      $(data).each(function(i, diary) {
-		$('.diary-list').find('.row').append('<div class="col-sm-3 card' + i + '">');
+		$('.diary-list').append('<div class="row">');
+		//$('<div class="row">').appendTo('.diary-list').hide().addClass('is-animated').fadeIn(1000);
+	        $(data).each(function(i, diary) {
+		    $('<div class="col-sm-3 card' + i + '">').appendTo('.diary-list > .row').hide().delay(i*100).addClass('is-animated').fadeIn(1000);
 		$('.card' + i).append('<div class="card mb-3" style="width: 15rem;">');
 		$('.card' + i).find('.card').append('<div class="card-block">');
 	        $('.card' + i).find('.card').find('.card-block').append('<h5 class="card-title">' + diary.display_date + '</h5>');
